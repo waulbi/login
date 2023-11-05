061c0ee4c5df17edcb9fe27e93e8ba5e9ab8d6fd
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-05 12:45:32 +0700

    keyword

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/j
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.0.9/config.js";
 
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
+wauthparam.keyword="aHR0cHM6Ly93YS5tZS82MjgzMTMxODk1MDAwP3RleHQ9d2g0dDVhdXRoMA==";
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
