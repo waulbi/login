1e28e67bb5ee858dfc0916c2cd3eb12fe48c2ce9
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-09-02 12:06:34 +0700

    setelah login redirect ke bagian pendaftaran

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/whatsauth.js";
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
 
-wauthparam.redirect="/auth/";
+wauthparam.redirect="https://wa.ulbi.ac.id/auth/";
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 //wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTgwMDAwNjAwMD90ZXh0PXdoNHQ1YXV0aDA=";
 wauthparam.keyword="aHR0cHM6Ly93YS5tZS82MjgzMTMxODk1MDAwP3RleHQ9d2g0dDVhdXRoMA==";
