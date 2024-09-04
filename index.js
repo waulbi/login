fba9d42a8a6fcf9090b033b99ded7a839a60579f
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-09-04 10:54:35 +0700

    ganti wauthparam.keyword

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/confi
 
 wauthparam.redirect="https://wa.ulbi.ac.id/auth/";
 wauthparam.auth_ws="d3NzOi8vZ3cudWxiaS5hYy5pZC93cy93aGF0c2F1dGgvcHVibGlj";
-// wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5OTk4MDgwMDE/dGV4dD13aDR0NWF1dGgw";
-wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5OTE3NjAwNTA/dGV4dD13aDR0NWF1dGgw"; 
+wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5OTk4MDgwMDE/dGV4dD13aDR0NWF1dGgw";
+// wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5OTE3NjAwNTA/dGV4dD13aDR0NWF1dGgw"; 
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
